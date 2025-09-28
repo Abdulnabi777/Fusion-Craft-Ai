0b7e469dac15e0d9f1962eb2563759e3e763a361
Abdul Nabi <abdulnabi9257@.com>
2025-09-28 19:17:42 +0530

    add 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
         <Route path='/ai' element={<Layout />}>
         <Route index element={<Dashboard />} />
         <Route path='write-article' element={<WriteArticle />} />
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,18 +2,24 @@ import React from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 import { ArrowRight } from 'lucide-react'
+import {useClerk, UserButton, useUser} from '@clerk/clerk-react'
 
 const Navbar = () => {
     const navigate = useNavigate()
+    const {user}= useClerk()
+    const {openSignIn} = useClerk()
   return (
     <div className='fixed z-5 w-full background-blur-2xl flex justify-between items-center cursor-pointer py-3 px-4 sm:px-20 xl:px-32'>
 <img src={assets.logo} alt="logo" className="w-32 sm:w-44" onClick={() => navigate('/')} />
 
-        
-<button className="flex items-center gap-2 rounded-full text-sm font-medium cursor-pointer bg-[var(--color-primary)] hover:bg-[color-mix(in srgb, var(--color-primary) 90%, white)] transition-colors text-white px-10 py-2.5">
+{
+  user ? <UserButton /> : (
+       
+<button onClick={openSignIn} className="flex items-center gap-2 rounded-full text-sm font-medium cursor-pointer bg-[var(--color-primary)] hover:bg-[color-mix(in srgb, var(--color-primary) 90%, white)] transition-colors text-white px-10 py-2.5">
   Get Started <ArrowRight className="w-4 h-4" />
 </button>
- 
+ )
+}
     </div>
   )
 }
